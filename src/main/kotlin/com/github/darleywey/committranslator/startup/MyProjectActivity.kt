48498fc74d156b2248d34872c0892ef8fdef18a9
GitHub Action <action@github.com>
2026-01-19 20:20:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/darleywey/committranslator/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/darleywey/committranslator/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.darleywey.committranslator.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
